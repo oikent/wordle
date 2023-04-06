38995c924a379fce85fd847be36d0a45d48b5f1b
oikent <christianstickel@Hotmail.com>
2023-04-06 22:29:57 +1000

    adding functionality

--- script.js
+++ script.js
@@ -9,14 +9,47 @@
 //     h1.textContent = word
 //  }
 
-// button.addEventListener('click',getWord)
+const getWords = async () => {
+    const response = await fetch("./words.json");
+    const words = await response.json();
+    const word = words[Math.floor(Math.random() * words.length)];
+    return word;
+};
 
-const getWords = async () =>{
-    const response = await fetch("./words.json")
-    const words = await response.json()
-    const word = words[Math.floor(Math.random() *words.length)]
+getWords();
+
+const attemptOneCells = document.querySelectorAll(".attempt-one-cell");
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const third = document.querySelector(".third");
+const fourth = document.querySelector(".fourth");
+const fifth = document.querySelector(".fifth");
+const cells = document.querySelectorAll(".cell");
+
+let word = "alpha";
+let guess = [];
+let count = 0;
+function addLetter(e) {
+    if (e.key === "Backspace") {
+        // guess.pop();
+        if (count > 0) {
+            count--;
+            cells[count].value = "";
+        }
+    } else if (e.key !== "Backspace" && e.key !== "Enter") {
+        cells[count].value = e.key;
+        // guess.push(cells[count].value);
+        count++;
+    } else if (count % 5 === 0 && e.key === "Enter") {
+        console.log(count);
+        for (let i = 0; i < cells.length; i++) {
+            if (cells[i].value === word[i]) {
+                cells[i].classList.add("correct");
+            } else if (word.includes(cells[i].value) && cells[i].value !== "") {
+                cells[i].classList.add("present");
+            }
+        }
+    }
 }
 
-getWords()
-
-// set cell to uppercase
+document.addEventListener("keydown", addLetter);
