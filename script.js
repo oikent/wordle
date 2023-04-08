7274a9b91819f632493e8d461e4d9628aaaa6c06
oikent <christianstickel@Hotmail.com>
2023-04-08 20:17:46 +1000

    added functionality

--- script.js
+++ script.js
@@ -9,47 +9,130 @@
 //     h1.textContent = word
 //  }
 
+let guess = [];
+let count = 0;
+const invalidKeys = [
+    "Backspace",
+    "Escape",
+    "Enter",
+    "Tab",
+    "Shift",
+    "Control",
+    "Alt",
+    "[",
+    "]",
+    "'",
+    ";",
+    "/",
+    ".",
+    ",",
+    "=",
+    "-",
+    "0",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    "CapsLock",
+    "Fn",
+    " ",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowUp",
+    "\\",
+    "F1",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "F6",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+];
+
+const instructions = document.querySelector(".help");
+const container = document.querySelector(".container");
+const keyboard = document.querySelector(".keyboard");
+const keys = document.querySelectorAll(".keys");
+
+console.log(keys[0].textContent);
+instructions.addEventListener("click", () => {
+    instructions.style.display = "none";
+    keyboard.style.display = "grid";
+    container.style.display = "grid";
+});
+
 const getWords = async () => {
     const response = await fetch("./words.json");
     const words = await response.json();
-    const word = words[Math.floor(Math.random() * words.length)];
-    return word;
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    let wordToString = randomWord.toString().toUpperCase();
+    let letterArr = [];
+    for (w of wordToString) {
+        letterArr.push(w);
+    }
+    return letterArr;
 };
 
-getWords();
-
-const attemptOneCells = document.querySelectorAll(".attempt-one-cell");
-const first = document.querySelector(".first");
-const second = document.querySelector(".second");
-const third = document.querySelector(".third");
-const fourth = document.querySelector(".fourth");
-const fifth = document.querySelector(".fifth");
-const cells = document.querySelectorAll(".cell");
-
-let word = "alpha";
-let guess = [];
-let count = 0;
-function addLetter(e) {
-    if (e.key === "Backspace") {
-        // guess.pop();
-        if (count > 0) {
-            count--;
-            cells[count].value = "";
-        }
-    } else if (e.key !== "Backspace" && e.key !== "Enter") {
-        cells[count].value = e.key;
-        // guess.push(cells[count].value);
-        count++;
-    } else if (count % 5 === 0 && e.key === "Enter") {
-        console.log(count);
-        for (let i = 0; i < cells.length; i++) {
-            if (cells[i].value === word[i]) {
-                cells[i].classList.add("correct");
-            } else if (word.includes(cells[i].value) && cells[i].value !== "") {
-                cells[i].classList.add("present");
+getWords().then((word) => {
+    function addLetter(e) {
+        const cells = document.querySelectorAll(".completed");
+        if (!invalidKeys.includes(e.key) && count !== 5) {
+            cells[count].value = e.key.toUpperCase();
+            guess.push(cells[count].value);
+            count++;
+        } else if (e.key === "Backspace") {
+            if (count > 0) {
+                count--;
+                cells[count].value = "";
+                guess.pop();
             }
+        } else if (count % 5 === 0 && count !== 0 && e.key === "Enter") {
+            for (let i = 0; i < count; i++) {
+                setTimeout(() => {
+                    if (guess[i] === word[i]) {
+                        cells[i].classList.add("correct");
+                        cells[i].classList.remove("completed");
+                    } else if (word.includes(guess[i])) {
+                        cells[i].classList.add("present");
+                        cells[i].classList.remove("completed");
+                    } else {
+                        cells[i].classList.add("incorrect");
+                        cells[i].classList.remove("completed");
+                    }
+                }, 300 * i);
+            }
+            setTimeout(() => {
+                const g = [guess.join()];
+                const w = [word.join()];
+                console.log(g, w);
+                if (g[0] === w[0]) {
+                    console.log("winner");
+                }
+                guess = [];
+                count = 0;
+            }, 300 * 6);
         }
     }
-}
-
-document.addEventListener("keydown", addLetter);
+    document.addEventListener("keydown", addLetter);
+});
